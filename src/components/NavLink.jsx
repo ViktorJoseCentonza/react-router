889a182fc6a274f88eb0a5458a1372e81dd7b15a
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-04-01 14:50:52 +0200

    add logo

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -3,9 +3,17 @@ import { Link, NavLink } from "react-router-dom";
 export default function Navlink() {
     return (
         <nav>
-            <NavLink to='/'>Home Page</NavLink>
-            <NavLink to='/about-us'>About Us</NavLink>
-            <NavLink to='/posts-page'>Posts Page</NavLink>
+
+            <NavLink to='/' id="logo-wrapper">
+                <img src="./fabio_spin.gif" alt="fabio_spin" />
+            </NavLink>
+
+            <div>
+                <NavLink to='/'>Home Page</NavLink>
+                <NavLink to='/about-us'>About Us</NavLink>
+                <NavLink to='/posts-page'>Posts Page</NavLink>
+            </div>
+
         </nav>
     )
 }
